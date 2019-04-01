c4acf8f479b3734159efac6c8fc1ec644c60c41b
bmulla <bmulla@qatar.cmu.edu>
2019-04-01 16:39:25 +0000

    weird error will restart cloud 9

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -3,12 +3,12 @@ class DemosController < ApplicationController
     end
     
     def create
-       employee = Employee.find_by(phone: params[:demo][:phone]) 
+       employee = Employee.find_by(ssn: params[:demo][:ssn])
        if employee && employee.authenticate(params[:demo][:password])
            login(employee)
            redirect_to employee
        else
-           flash.now[:danger] = "Invalid phone or password"
+           flash.now[:danger] = "Invalid Social Security Number or password"
            render 'new'
        end
     end
